3d0aa18330c7eb3f7d22377d29c7f0d7f742b2a8
T M Mohammed Sagheer <tmmsagheer@gmail.com>
2021-01-04 12:47:16 +0530

    Included code to enable app insights

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -11,5 +11,8 @@ name = ""
 service = Webservice(name=name, workspace=ws)
 logs = service.get_logs()
 
+#Enable Application Insights
+service.update(enable_app_insights=True)
+
 for line in logs.split('\n'):
     print(line)
